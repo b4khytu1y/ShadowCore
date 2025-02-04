ac7f596a29bbce828cea3fb783649c3e74d948d3
B4khytu1y <bakhytulynurzhan@gmail.com>
2025-02-04 09:37:04 +0500

    Добавьте файлы проекта.

---
+++ Main_page.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShadowCore
+{
+    public partial class Main_page : Form
+    {
+        public Main_page()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
